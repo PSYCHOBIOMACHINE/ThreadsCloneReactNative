74652df3e2c5bf79614b452c199e7cd9a56d001f
PSYCHOBIOMACHINE <mind.machine.19@gmail.com>
2025-08-23 10:36:19 -0400

    new post modal UI

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -4,7 +4,7 @@ export default function ProtectedLayout() {
     return (
     <Stack >
         <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-        <Stack.Screen name='new' options={{ presentation: 'modal', animation: 'slide_from_bottom'}} />
+        <Stack.Screen name='new' options={{ title: "New Thread", presentation: 'modal', animation: 'slide_from_bottom'}} />
     </Stack>
     );
 } 
--- src/app/(protected)/new.tsx
+++ src/app/(protected)/new.tsx
@@ -1,9 +1,34 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Pressable, KeyboardAvoidingView, Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function NewPostScreen() {
+
+    const [text, setText] = useState('');
+
     return (
-        <View>
-            <Text className = 'text-white'>New Post</Text>
-        </View>
+        <SafeAreaView className={'p-4 flex-1'}>
+            <KeyboardAvoidingView 
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+                className="flex-1"
+                keyboardVerticalOffset= {Platform.OS === 'ios' ? 140 : 0}
+                >
+                <Text className = 'text-white text-2xl font-bold'>PSYCHOBIOMACHINE</Text>
+                <TextInput
+                    value={text}
+                    onChangeText={setText}
+                    placeholder="What's on your mind?"
+                    className="text-white text-lg"
+                    multiline
+                    numberOfLines={4}
+                
+                />
+                <View className="mt-auto">
+                    <Pressable onPress={()=>{console.log("post: ", text)}} className = "bg-white p-3 px-6 self-end rounded-full" >
+                        <Text className = "text-black font-bold">Post</Text>
+                    </Pressable>
+                </View>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     );
 }
